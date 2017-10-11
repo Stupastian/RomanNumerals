2936670ce3baca957d96c610a1f1c961002fa720
Mikko Ruotsalainen <rooyeb@hotmail.com>
2017-10-11 19:02:35 +0300

    Assignment Finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,117 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		
+		romanNum = romanNum.toUpperCase();
+		if(testRepetitiveIXCM(romanNum)) return -1;
+		if(testRepetitiveVLD(romanNum)) return -2;
+		if(testAllowedSubstractionsIXC(romanNum)) return -3;
+		if(testAllowedSubstractionsOne(romanNum)) return -4;
+		if(testNoSubstractionAllowedVLD(romanNum)) return -5;
+		if(!romanNum.matches("^[IVXLCDM]+$") && romanNum != "") return -6;
+		
+		return calculateString(convertToNumberArray(romanNum));
 		
 	}
+	
+	private int getMainNumberValue(char pieceOfString){
+		int result = 0;
+		
+		switch (pieceOfString){
+			case 'I': 	result = 1;
+						break;
+			case 'V':	result = 5;
+						break;
+			case 'X':   result = 10;
+						break;
+			case 'L':   result = 50;
+						break;
+			case 'C':   result = 100;
+						break;
+			case 'D': 	result = 500;
+						break;
+			case 'M':   result = 1000;
+						
+		}
+		return result;
+	}
+	
+	private boolean testRepetitiveIXCM(String romanNum){
+		boolean test = false;
+		int counter = 1;
+		for(int i = 0; i < romanNum.length(); i++){
+			if(i >= 1 && romanNum.charAt(i) == romanNum.charAt(i-1)) {
+				counter++;
+				if(counter >= 4) test = true;
+			}
+			else counter = 1;
+		}
+		return test;
+	}
+	
+	private boolean testRepetitiveVLD(String romanNum){
+		boolean test = false;
+		int counter = 1;
+		for(int i = 0; i < romanNum.length(); i++){
+			if(i >= 1 && romanNum.charAt(i) == romanNum.charAt(i-1) && "VLD".indexOf(romanNum.charAt(i)) >= 0) {
+				counter++;
+				if(counter >= 2) test = true;
+			}
+			else counter = 1;
+		}
+		return test;
+	}
+	
+	private boolean testAllowedSubstractionsIXC(String romanNum){
+		boolean test = false;
+
+		for(int i = 1; i < romanNum.length(); i++){
+			if(romanNum.charAt(i-1) == 'I' && "LCDM".indexOf(romanNum.charAt(i)) >= 0) test = true;
+			if(romanNum.charAt(i-1) == 'X' && "DM".indexOf(romanNum.charAt(i)) >= 0) test = true;
+		}	
+		return test;
+	}
+	
+	private boolean testAllowedSubstractionsOne(String romanNum){
+		boolean test = false;
+
+		for(int i = 2; i < romanNum.length(); i++){
+			if(romanNum.charAt(i-2) == 'I' && romanNum.charAt(i-1) == 'I' && "VX".indexOf(romanNum.charAt(i)) >= 0) test = true;
+			if(romanNum.charAt(i-2) == 'X' && romanNum.charAt(i-1) == 'X' && "LC".indexOf(romanNum.charAt(i)) >= 0) test = true;
+			if(romanNum.charAt(i-2) == 'C' && romanNum.charAt(i-1) == 'C' && "DM".indexOf(romanNum.charAt(i)) >= 0) test = true;
+		}	
+		return test;
+	}
+	
+	private boolean testNoSubstractionAllowedVLD(String romanNum){
+		boolean test = false;
+
+		for(int i = 1; i < romanNum.length(); i++){
+			if(romanNum.charAt(i-1) == 'V' && romanNum.charAt(i) != 'I') test = true;
+			if(romanNum.charAt(i-1) == 'L' && "IVX".indexOf(romanNum.charAt(i)) == -1) test = true;
+			if(romanNum.charAt(i-1) == 'D' && romanNum.charAt(i) == 'M') test = true;
+		}	
+		return test;
+	}
+	
+	private int calculateString(int[] numbersArray){
+		int result = 0;
+		
+		for(int i=0; i < numbersArray.length; i++){
+			if(i < numbersArray.length-1 && numbersArray[i] < numbersArray[i+1]) result-=numbersArray[i];
+			else result += numbersArray[i];
+		}	
+		return result;
+	}
+	
+	private int[] convertToNumberArray(String romanNum){
+		int[] numbersArray = new int[10];
+
+		for(int i = 0; i < romanNum.length(); i++){
+			numbersArray[i] = getMainNumberValue(romanNum.charAt(i));
+		}
+		return numbersArray;
+	}
+	
+	
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -1,12 +1,153 @@
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestRomanNumerals {
 
-	@Test
+	@Ignore
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	
+	
+	@Test
+	public void testRomanNumeralsIXCMRepeatRules(){
+		//setup
+		RomanNumerals testable = new RomanNumerals();
+		
+		//Assert
+		
+		assertEquals("Input IIII should return error code -1", -1, testable.convertToInteger("IIII"));
+		assertEquals("Input XXXX should return error code -1", -1, testable.convertToInteger("XXXX"));
+		assertEquals("Input CCCC should return error code -1", -1, testable.convertToInteger("CCCC"));
+		assertEquals("Input MMMM should return error code -1", -1, testable.convertToInteger("MMMM"));
+	}
+	
+	@Test
+	public void testRomanNumeralsVLDRepeatRules(){
+		//Setup
+		RomanNumerals testable = new RomanNumerals();
+		
+		//Assert
+		assertEquals("Input VV should return error code -2", -2, testable.convertToInteger("VV"));
+		assertEquals("Input VV should return error code -2", -2, testable.convertToInteger("LL"));
+		assertEquals("Input VV should return error code -2", -2, testable.convertToInteger("DD"));
+	}
+	
+	@Test
+	public void testRomanNumeralsIXCSubstractRules(){
+		//Setup
+		RomanNumerals testable = new RomanNumerals();
+		
+		//Assert
+		assertEquals("Input IL should return error code -3", -3, testable.convertToInteger("IL"));
+		assertEquals("Input IC should return error code -3", -3, testable.convertToInteger("IC"));
+		assertEquals("Input IC should return error code -3", -3, testable.convertToInteger("ID"));
+		assertEquals("Input IC should return error code -3", -3, testable.convertToInteger("IM"));
+		assertEquals("Input XD should return error code -3", -3, testable.convertToInteger("XD"));
+		assertEquals("Input XM should return error code -3", -3, testable.convertToInteger("XM"));
+
+	}
+	
+	@Test
+	public void testRomanNumeralsOnlyOneSubstraction(){
+		//Setup
+		RomanNumerals testable = new RomanNumerals();
+		
+		//Assert
+		assertEquals("Input IIV should return error code -4", -4, testable.convertToInteger("IIV"));
+		assertEquals("Input IIX should return error code -4", -4, testable.convertToInteger("IIX"));
+		assertEquals("Input XXL should return error code -4", -4, testable.convertToInteger("XXL"));
+		assertEquals("Input XXC should return error code -4", -4, testable.convertToInteger("XXC"));
+		assertEquals("Input CCD should return error code -4", -4, testable.convertToInteger("CCD"));
+		assertEquals("Input CCM should return error code -4", -4, testable.convertToInteger("CCM"));
+		
+	}
+	@Test
+	public void testRomanNumeralsNotSubtractedVLD(){
+		//Setup
+		RomanNumerals testable = new RomanNumerals();
+		
+		//Assert
+		assertEquals("Input VX should return error code -5", -5, testable.convertToInteger("VX"));
+		assertEquals("Input LC should return error code -5", -5, testable.convertToInteger("LC"));
+		assertEquals("Input DM should return error code -5", -5, testable.convertToInteger("DM"));
+
+		
+	}
+	
+	@Test
+	public void testRomanNumeralsWithBasicElements() {
+		//setup
+		RomanNumerals testable = new RomanNumerals();
+		
+		//Assert
+		assertEquals("input I should return 1", 1, testable.convertToInteger("I"));
+		assertEquals("input X should return 10", 10, testable.convertToInteger("X"));
+		assertEquals("input L should return 50", 50, testable.convertToInteger("L"));
+		assertEquals("input C should return 100", 100, testable.convertToInteger("C"));
+		assertEquals("input D should return 500", 500, testable.convertToInteger("D"));
+		assertEquals("input M should return 1000", 1000, testable.convertToInteger("M"));
+		
+	}
+	
+	@Test
+	public void testRomanNumeralsAddTogether(){
+		//Setup
+		RomanNumerals testable = new RomanNumerals();
+		
+		//Assert
+		assertEquals("Input MMMIII should return 3003", 3003, testable.convertToInteger("MMMIII"));
+	
+	}
+	
+	@Test
+	public void testRomanNumeralsSubstraction(){
+		//Setup
+		RomanNumerals testable = new RomanNumerals();
+		
+		//Assert
+		assertEquals("Input IV should return 4", 4, testable.convertToInteger("IV"));
+		assertEquals("Input IX should return 9", 9, testable.convertToInteger("IX"));
+		assertEquals("Input XL should return 40", 40, testable.convertToInteger("XL"));
+		assertEquals("Input XC should return 90", 90, testable.convertToInteger("XC"));
+		assertEquals("Input CD should return 400", 400, testable.convertToInteger("CD"));
+		assertEquals("Input CM should return 900", 900, testable.convertToInteger("CM"));
+
+		
+	}
+	
+	@Test
+	public void testRomanNumeralsTotal(){
+		//Setup
+		RomanNumerals testable = new RomanNumerals();
+		
+		//Assert
+		assertEquals("Input MMMCMXCIX should return 3999", 3999, testable.convertToInteger("MMMCMXCIX"));
+		assertEquals("Empty Input should return 0", 0, testable.convertToInteger(""));
+	}
+	
+	@Test
+	public void testRomanNumeralsShouldAcceptLowerCase(){
+		//Setup
+		RomanNumerals testable = new RomanNumerals();
+		
+		//Assert
+		assertEquals("Input mmmcmxcix should return 3999", 3999, testable.convertToInteger("mmmcmxcix"));
+	}
+	
+	@Test
+	public void testRomanNumeralsOnlyContainMDCLXVI(){
+		//Setup
+		RomanNumerals testable = new RomanNumerals();
+		
+		//Assert
+		assertEquals("Input MMFM should return -6", -6, testable.convertToInteger("MMFM"));
+		assertEquals("Input MF should return -6", -6, testable.convertToInteger("MF"));
+		assertEquals("Input FM should return -6", -6, testable.convertToInteger("FM"));
+		
 	}
 
 }
